e9c376eeb702ff770b3fbd44519b755169b68503
key2me <kikg1993@gmail.com>
2014-11-25 23:57:50 +0545

    add space

--- area.rb
+++ area.rb
@@ -14,6 +14,7 @@ end
 mytask= Task.new
 puts "Square = #{mytask.square(5)}" 
 puts "Circle = #{mytask.circle(7)}"
-puts "Rectangle =#{mytask.rect(2,3)}"
+puts "Rectangle = #{mytask.rect(2,3)}"
 puts "Hello World"
 
+ 
