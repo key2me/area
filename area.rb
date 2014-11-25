5a8541679043ca2ec47245dfa1e224574e469d69
key2me <kikg1993@gmail.com>
2014-11-25 23:28:53 +0545

    some changes

---
+++ area.rb
@@ -0,0 +1,19 @@
+class Task
+
+	def square(n)
+		n*n
+	end
+	def circle(r)
+		22/7*r*r
+	end
+	def rect(l,b)
+		l*b
+	end
+end
+
+mytask= Task.new
+puts "Square = #{mytask.square(5)}" 
+puts "Circle = #{mytask.circle(7)}"
+puts "Rectangle =#{mytask.rect(2,3)}"
+
+
